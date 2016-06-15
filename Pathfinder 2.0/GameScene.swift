d73adae1da0231ada00a0fb55908dbe0cb8bfce2
Justin Matsnev <jmatsnev@gmail.com>
2016-06-14 17:39:32 -0700

    First commit

---
+++ Pathfinder 2.0/GameScene.swift
@@ -0,0 +1,24 @@
+//
+//  GameScene.swift
+//  Pathfinder 2.0
+//
+//  Created by Justin Matsnev on 6/14/16.
+//  Copyright (c) 2016 Justin Matsnev. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+      
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+       
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
